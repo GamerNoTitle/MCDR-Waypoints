379553efda4f9fc376a483bfea3c820ff505c6f0
GamerNoTitle <bili33@87ouo.top>
2020-07-19 21:50:57 +0800

    加入删除提示

--- Waypoints.py
+++ Waypoints.py
@@ -133,6 +133,7 @@ def delete(server,info,point):
             data=[name[i],x[i],y[i],z[i],dimension[i]]
             append_csv(path,data)
         refresh_list()
+        server.tell(info.player, '§b[Waypoints]§r名为§d{}的导航点已经删除！'.format(point))
 
 
 def showdetail(server,info,point):
