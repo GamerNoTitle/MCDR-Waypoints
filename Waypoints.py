33d466d37f5a0693bab5cae0ccb8e7b28a62f6fb
GamerNoTitle <bili33@87ouo.top>
2020-07-19 21:24:39 +0800

    写完收工!

--- Waypoints.py
+++ Waypoints.py
@@ -43,11 +43,10 @@ def refresh_list():
         z.append(i[3])
         dimension.append(i[4])
     complicated==False
-refresh_list()
+
 def create_csv(path):
-    with open(path,"w+",newline="",encoding="gbk") as file:
-        csv_file = csv.writer(file)
-        csv_file.writerow('')
+    with open(path,"w+",newline='',encoding="gbk") as file:
+        file.close()
 
 def append_csv(path,data):
     with open(path,"a+",newline='',encoding="gbk") as file:
@@ -105,7 +104,34 @@ def is_duplicated(point):
             complicated=True
 
 def delete(server,info,point):
-    None
+    point_pos=None
+    for i in range(0,len(name)):
+        if point == name[i]:
+            point_pos=i
+    if point_pos == None:
+        server.tell(info.player, '§b[Waypoints]§4未找到名为§d{}§4的路径点！'.format(point))
+    else:
+        for i in range(point_pos,len(name)):
+            try:
+                name[i] = name[i+1]
+                x[i] = x[i+1]
+                y[i] = y[i+1]
+                z[i] = z[i+1]
+                dimension[i] = dimension[i+1]
+            except:
+                None
+        del(name[len(name)-1])
+        del(x[len(x)-1])
+        del(y[len(y)-1])
+        del(z[len(z)-1])
+        del(dimension[len(dimension)-1])
+        os.remove(path)
+        create_csv(path)
+        for i in range(0,len(name)):
+            data=[name[i],x[i],y[i],z[i],dimension[i]]
+            append_csv(path,data)
+        refresh_list()
+
 
 def showdetail(server,info,point):
     is_exist=False
@@ -139,7 +165,7 @@ def search(server,info,point,dim):
             if point in name[i]:
                 result.append(name[i])
         if result == []:
-            server.tell(info.player, '§b[Waypoints]§r暂时没有含有§d{}§r关键词的路径点哦~'.format(point))
+            server.tell(info.player, '§b[Waypoints]§4暂时没有含有§d{}§4关键词的路径点哦~'.format(point))
         else:
             server.tell(info.player, '§b[Waypoints]§r含有关键词§d{}§r的路径点有§6{}'.format(point,result))
             server.tell(info.player, '§b[Waypoints]§r你可以使用§b!!wp show <name> §r来展示导航点的相关信息')
@@ -148,7 +174,7 @@ def search(server,info,point,dim):
             if point in name[i] and int(dimension[i]) == dim:
                 result.append(name[i])
         if result == []:
-            server.tell(info.player, '§b[Waypoints]§r暂时没有含有§d{}§r关键词的路径点哦~'.format(point))
+            server.tell(info.player, '§b[Waypoints]§4暂时没有含有§d{}§4关键词的路径点哦~'.format(point))
         else:
             server.tell(info.player, '§b[Waypoints]§r含有关键词§d{}§r的路径点有§6{}'.format(point,result))
             server.tell(info.player, '§b[Waypoints]§r你可以使用§b!!wp show <name> §r来展示导航点的相关信息')
@@ -162,11 +188,14 @@ def dimshow(server,info,dim):
         dimension_name = '§2末地'
     if int(dim) == -1:
         dimension_name = '§c地狱'
+    result=[]
     for i in range(0,len(name)):
-        result=[]
         if int(dimension[i]) == dim:
             result.append(name[i])
-        server.tell(info.player, '§b[Waypoints]§r在维度{}§r里共有导航点§d{}§r个，列表如下：{}'.format(dimension_name,len(result),result))
+    server.tell(info.player, '§b[Waypoints]§r在维度{}§r里共有导航点§d{}§r个，列表如下：{}'.format(dimension_name,len(result),result))
+
+def on_load(server, old_module):
+    refresh_list()
 
 def get_pos(server,info):
     PlayerInfoAPI = server.get_plugin_instance('PlayerInfoAPI')
@@ -178,9 +207,11 @@ def on_server_startup(server):
         refresh_list()
     else:
         create_csv(path)
-        refresh_list()
+
 
 def on_info(server,info):
+    if info.content == '!!create':
+        create_csv(path)
     if prefix in info.content or prefix_short in info.content:
         message=info.content.split()
         if info.content == prefix or info.content == prefix_short:
@@ -196,7 +227,12 @@ def on_info(server,info):
                 else:
                     add(server,info,message)
             if message[1] == 'del':
-                None
+                if len(message) == 2:
+                    server.tell(info.player, '§b[Waypoints]§4你必须输入要删除的路径点名字！')
+                elif len(message) == 3:
+                    delete(server,info,message[2])
+                else:
+                    server.tell(info.player, '§b[Waypoints]§4输入格式不正确！')
             
             if message[1] == 'reload':
                 try:
